1f0c0d7defcc186deaabf7f26a5a166a3a0b7cd6
M.Safi Ullah <100302433+m-safi-ullah@users.noreply.github.com>
2025-08-18 00:20:59 +0500

    Update backend URL in axios.jsx
    Deploying the changes on dashboard.render.com

--- client/src/baseURL/axios.jsx
+++ client/src/baseURL/axios.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // Configure axios globally
-axios.defaults.baseURL = "http://localhost:4400";
+axios.defaults.baseURL = "https://skill-flow-backend-dhsq.onrender.com";
 axios.defaults.timeout = 10000;
 axios.defaults.headers.common["Content-Type"] = "application/json";
 axios.defaults.withCredentials = true;
